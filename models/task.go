d2b4a8340d93b79bca2f34dfcacb373a0be71d0d
Arthur Ferreira <87419255+Naroshh@users.noreply.github.com>
2024-10-29 10:16:58 +0200

    changes

--- models/task.go
+++
@@ -1,8 +0,0 @@
-package models
-
-// Task represents a single task in our task manager
-type Task struct {
-	ID        int    // Unique ID for the task
-	Title     string // Title or description of the task
-	Completed bool   // Whether the task is completed
-}
